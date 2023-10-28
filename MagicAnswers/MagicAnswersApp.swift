eddb44877317b1f699c5220233bb90c4c52b9bae
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-10-28 15:04:27 -0500

    Initial Commit

---
+++ MagicAnswers/MagicAnswersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicAnswersApp.swift
+//  MagicAnswers
+//
+//  Created by Chuck Condron on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MagicAnswersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
